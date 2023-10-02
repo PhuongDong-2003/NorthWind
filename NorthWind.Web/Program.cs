c2787e5a02ae01e84869d43fbefda207c77ffd46
PhuongDong-2003 <nguyenphuongdong4102003@gmail.com>
2023-10-02 16:17:43 +0700

    update program

--- NorthWind.Web/Program.cs
+++ NorthWind.Web/Program.cs
@@ -4,9 +4,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-var configSection = builder.Configuration.GetRequiredSection(ApiUrlsConfiguration.CONFIG_NAME);
-builder.Services.Configure<ApiUrlsConfiguration>(configSection);
-var baseUrlConfig = configSection.Get<ApiUrlsConfiguration>();
+// cách 1 tham khảo trên learn_microsoft
+builder.Services.Configure<ApiUrlsConfiguration>(
+builder.Configuration.GetSection(ApiUrlsConfiguration.CONFIG_NAME));
+
+// Cách 2 tham khảo project mẫu
+// var configSection = builder.Configuration.GetRequiredSection(ApiUrlsConfiguration.CONFIG_NAME);
+// builder.Services.Configure<ApiUrlsConfiguration>(configSection);
+// var baseUrlConfig = configSection.Get<ApiUrlsConfiguration>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

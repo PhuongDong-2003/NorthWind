438b635298b18374a78c76e60736765c6f4a1194
PhuongDong-2003 <nguyenphuongdong4102003@gmail.com>
2023-11-10 10:57:37 +0700

    update addcart

--- NorthWind.Shop/wwwroot/js/product.js
+++ NorthWind.Shop/wwwroot/js/product.js
@@ -1,10 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
 document.getElementById("searchInput").addEventListener("keyup", function (event) {
-    // Kiểm tra xem phím Enter (keyCode 13) đã được ấn
     if (event.key === "Enter") {
-        // Lấy giá trị từ trường nhập liệu
         var searchTerm = document.getElementById("searchInput").value;
 
-        // Sử dụng Ajax để gửi yêu cầu tìm kiếm đến Controller
         $.ajax({
             url: "/Home/Find",
             type: "POST",
@@ -14,29 +12,32 @@ document.getElementById("searchInput").addEventListener("keyup", function (event
         });
     }
 });
+});
 
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("AddToCart").addEventListener("click", function (event) {
-        var productId = document.querySelector('input[name="productId"]').value;
-        console.log("Product ID: " + productId);
-    
-        $.ajax({
-            url: "/Cart/AddCart",
-            type: "POST",
-            data: { productId: productId },
-            success: function (data) {
-                if (data.success) 
-                {
-                    // Hiển thị thông báo khi thêm vào giỏ hàng thành công
-                    alert("Add Cart Success");
-                } else {
+    var buttons = document.querySelectorAll("[name^='AddToCart']");
+    buttons.forEach(function (button) {
+        var productId = button.getAttribute("data-productid");
+        button.addEventListener("click", function (event) {
+            $.ajax({
+                url: "/Cart/AddCart",
+                type: "POST",
+                data: { productId: productId },
+                success: function (data) {
+                    if (data.success) {
+                        // Hiển thị thông báo khi thêm vào giỏ hàng thành công
+                        alert("Add Cart Success");
+                    } else {
+                        alert("Add Cart failed");
+                    }
+                },
+                error: function (error) {
                     alert("Add Cart failed");
                 }
-            },
-            error: function (error) {
-                alert("Add Cart failed");
-            }
+            });
         });
     });
 });
+
+
 
